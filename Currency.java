e2a4271af4b5bc20e9a6df087787e9a77fb6d070
jcas96 <162757478+jcas96@users.noreply.github.com>
2024-05-16 10:05:06 -0700

    Update Currency.java

--- Currency.java
+++ Currency.java
@@ -3,23 +3,17 @@ import java.util.TreeMap;
 
 public class Currency {
 
-	
 	public static String[] currencyLine;
-
-
 	
 	static Map<String, Double> worldCurrencies=new TreeMap<String, Double>();
 
 	public Currency(String string, double parseDouble) {
-		// TODO Auto-generated constructor stub
 	}
 	public Currency() {
 		
 	}
-	
 
 	public static double convertToUSD(Map<String, Double> m, String code, double amount) {
-		
 		double c = 1;
 		if(m.size()>0) {
 			c = m.get(code);
